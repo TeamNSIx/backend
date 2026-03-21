a1cc15f9bd28c1362c0eeca01cb051916f8f44af
drYoyo322 <deeruptxone@gmail.com>
2026-03-21 12:46:29 +0300

    Feature/database setup (#6)
    Co-authored-by: Данис <danis@MacBook-Pro-Danis.local>

--- src/app/models/embedding.py
+++ src/app/models/embedding.py
@@ -6,6 +6,8 @@ from pgvector.sqlalchemy import Vector
 from sqlalchemy import Column
 from sqlmodel import Field, SQLModel
 
+EMBEDDING_PARAM = 1536
+
 
 class Embedding(SQLModel, table=True):
     __tablename__ = 'embeddings'
@@ -14,7 +16,7 @@ class Embedding(SQLModel, table=True):
 
     fragment_id: UUID = Field(foreign_key='source_fragments.id')
 
-    embedding: Optional[list] = Field(sa_column=Column(Vector(1536)))
+    embedding: Optional[list] = Field(sa_column=Column(Vector(EMBEDDING_PARAM)))
 
     model_name: Optional[str] = None
 
